ab8b190077f97b39b3fc40a3448f2b49cb4b2451
Nikhil-X-codes <nagarn2005@gmail.com>
2025-07-19 22:19:18 +0530

    refactor: Simplify Vite configuration by removing unused environment variables and proxy settings

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,27 +1,11 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig(({ mode }) => {
-
-  const env = loadEnv(mode, process.cwd())
-
-  const socketUrl = env.VITE_SOCKET_URL
-
-  return {
-    server: {
-      proxy: {
-        '/users': socketUrl,
-        '/videos': socketUrl,
-        '/likes': socketUrl,
-        '/comments': socketUrl,
-        '/subscriptions': socketUrl,
-        '/playlists': socketUrl,
-      }
-    },
-    build: {
-      outDir: 'dist' 
-    },
-    plugins: [react(), tailwindcss()],
-  }
+export default defineConfig({
+  build: {
+    outDir: 'dist'
+  },
+  plugins: [react(), tailwindcss()],
 })
+
